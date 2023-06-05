2a5382e921a92fbf7bc1700b09bbfbefd54ad10c
RockyBaura <251drougnov@gmail.com>
2023-06-05 15:04:29 +0600

    Eye icon visibility update

--- sign_up.js
+++ sign_up.js
@@ -1,136 +1,94 @@
-// Function to validate the password
-function validatePassword(password) {
-    console.log("validatePassword function run");
+// Show toggle icon on focus of password input
+function lHCgG(event) {
+    // get the target of the event
+    let element = event.target;
 
-    // Minimum length of 8 characters
-    if (password.length < 8) {
-        return false;
+    // Remove the password error prompt
+    var unq_oKTLa = document.querySelector(".unq_oKTLa");
+    unq_oKTLa.classList.add("dspl_dWotM");
+
+    // find parent div .form-field of the event target
+    let formField = element.closest(".form-field");
+    if (!formField) {
+        console.error("closest form field not found");
+        return;
     }
 
-    // Must contain at least one uppercase letter, one lowercase letter, and one number
-    if (
-        !/[A-Z]/.test(password) ||
-        !/[a-z]/.test(password) ||
-        !/[0-9]/.test(password)
-    ) {
-        return false;
+    // find password input within the form field
+    let passwordInput = formField.querySelector("input[data-type=password]");
+    if (!passwordInput) {
+        console.error("password input not found");
+        return;
     }
 
-    return true;
-}
+    // if the form field doesn't have the class 'first_focus'
+    if (!formField.classList.contains("first_focus")) {
+        // add the class 'first_focus'
+        formField.classList.add("first_focus");
 
-// Function to scroll to the password
-function scrolltoPassword() {
-    // Finding the element by attribute
-    var inputElements = document.getElementsByTagName("input");
-    for (var i = 0; i < inputElements.length; i++) {
-        // If the input element has 'data-type="password"', store it in passwordInput
-        if (inputElements[i].getAttribute("data-type") === "password") {
-            var passwordInput = inputElements[i];
-            break;
-        }
-    }
+        // add the class 'password_invisible'
+        formField.classList.add("password_invisible");
 
-    // Scrolling the page to the element
-    if (passwordInput !== undefined) {
-        // Get the dimensions of the password input field
-        var rect = passwordInput.getBoundingClientRect();
-        // Calculate the absolute top position of the password input field
-        var absoluteTop = rect.top + window.pageYOffset;
-        // Calculate the position which would place the password field in the middle of the page
-        var middle = absoluteTop - window.innerHeight / 2;
+        // set the type of the password input to 'password'
+        passwordInput.type = "password";
 
-        // Scroll to the middle position smoothly
-        window.scrollTo({
-            top: middle,
-            behavior: "smooth",
-        });
+        // find the eye icon wrapper within the form field
+        let eye_icon_wrapper = formField.querySelector(".piDFy");
+
+        // show the eye icon wrapper
+        eye_icon_wrapper.style.display = "block";
     }
 }
 
-// Show password prompt and prevent form submission
-var forms = document.getElementsByTagName("form");
-for (var i = 0; i < forms.length; i++) {
-    var form = forms[i];
-    // Find the first input field with 'data-type="password"' inside the form
-    var passwordInput = form.querySelector('[data-type="password"]');
+// Toggle visibility of password input on click of icon
+function piDFy(event) {
+    // get the target of the event
+    let element = event.target;
 
-    if (passwordInput) {
-        // If there's a password input in the form, add an event listener for form submission
-        (function (input) {
-            form.addEventListener("submit", function (event) {
-                // When the form is submitted, get the password from the input field
-                var passwordValue = input.value;
-                // If the password doesn't meet the validation criteria
-                if (!validatePassword(passwordValue)) {
-                    // Prevent form submission
-                    event.preventDefault();
+    // find parent div .form-field of the event target
+    let formField = element.closest(".form-field");
+    if (!formField) {
+        console.error("closest form field not found");
+        return;
+    }
 
-                    // Select the element with the class 'unq_oKTLa'
-                    var element = document.querySelector(".unq_oKTLa");
+    // find password input within the form field
+    let passwordInput = formField.querySelector("input[data-type=password]");
+    if (!passwordInput) {
+        console.error("password input not found");
+        return;
+    }
 
-                    // Remove the class 'dspl_dWotM' from the selected element - for display
-                    element.classList.remove("dspl_dWotM");
+    // if the form field has the class 'password_visible'
+    if (formField.classList.contains("password_visible")) {
+        // remove the class 'password_visible'
+        formField.classList.remove("password_visible");
 
-                    // Scroll to the password input field
-                    scrolltoPassword();
+        // add the class 'password_invisible'
+        formField.classList.add("password_invisible");
 
-                    console.log(
-                        "Password is not secure. Form submission prevented."
-                    );
-                }
-            });
-        })(passwordInput);
+        // set the type of the password input to 'password'
+        passwordInput.type = "password";
+
+        // focus on the password input
+        passwordInput.focus();
+    } else {
+        // remove the class 'password_invisible'
+        formField.classList.remove("password_invisible");
+
+        // add the class 'password_visible'
+        formField.classList.add("password_visible");
+
+        // set the type of the password input to 'text'
+        passwordInput.type = "text";
+
+        // focus on the password input
+        passwordInput.focus();
     }
 }
 
-// Add button loader
-form.addEventListener("submit", function (event) {
-    // Prevent form submission
-    event.preventDefault();
-
-    // Timeout function to delay execution
-    setTimeout(function () {
-        var forms = document.getElementsByTagName("form");
-        for (var i = 0; i < forms.length; i++) {
-            var form = forms[i];
-
-            // Check if the form has classes .xxx or .xxx
-            if (
-                form.classList.contains("xxx") ||
-                form.classList.contains("xxx")
-            ) {
-                continue; // Skip the logic for these forms
-            }
-
-            var requiredFields = form.querySelectorAll(":required");
-            var hasMissingFields = false;
-
-            // Check if all native required fields are entered
-            for (var j = 0; j < requiredFields.length; j++) {
-                if (!requiredFields[j].value) {
-                    hasMissingFields = true;
-                    break;
-                }
-            }
-
-            // Check if element with class 'unq_oKTLa' exists and has display none
-            var element = form.querySelector(".unq_oKTLa");
-            var shouldAddLoader =
-                element && getComputedStyle(element).display === "none";
-
-            //Add class 'loader' to the form's button if conditions are met
-            if (shouldAddLoader) {
-                buttonLoader(form);
-            }
-        }
-    }, 100);
+// Add an event listener for keydown to show the eye icon on any keypress
+passwordInput.addEventListener("keydown", function () {
+    let eye_icon_wrapper = formField.querySelector(".piDFy");
+    eye_icon_wrapper.style.display = "block";
 });
-
-function buttonLoader(form) {
-    var buttonContainer = form.querySelector(".btn-container");
-    var button = buttonContainer.querySelector("button");
-
-    button.classList.add("TpsXD", "white-loader");
-    button.style.position = "relative";
-}
