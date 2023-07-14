bf4ddbdc0d23f57b164a7ed1735f93b327a2cad1
RockyBaura <251drougnov@gmail.com>
2023-07-14 09:58:22 +0600

    Refactor: Seperated files & code readability

---
+++ dashboard.js
@@ -0,0 +1,73 @@
+// Selecting elements
+const hamburger = document.querySelector(".IhwpR"); // Hamburger button
+const sidebar = document.querySelector(".soGOI"); // Sidebar container
+const sidebarDropDownButtons = document.querySelectorAll(
+    ".sidebar-dropdown-btn"
+); // Sidebar dropdown buttons
+
+// open/close sidebar on hamburger click
+hamburger.addEventListener("click", toggleSidebar);
+
+//loop through all the sidebar dropdown buttons
+sidebarDropDownButtons.forEach((btn) => {
+    let dropDownContainer = btn.parentElement;
+    btn.addEventListener("click", toggleDropdown); // open/close dropdown on click
+
+    // Function to toggle dropdown visibility
+    function toggleDropdown(event) {
+        // Check if the clicked element is the icon-plus
+        const addBtn = dropDownContainer.querySelector(".icon-plus");
+        const isAddBtnClicked = addBtn.contains(event.target);
+        // open/close dropdown if the clicked element is the icon-plus
+        if (!isAddBtnClicked) {
+            dropDownContainer.classList.toggle("active");
+            updateSidebarActiveClass();
+            countDropDownContainerHeight(dropDownContainer);
+        }
+    }
+});
+
+// open/close sidebar by adding/removing class 'active'
+function toggleSidebar() {
+    sidebar.classList.toggle("active");
+    if (!sidebar.classList.contains("active")) {
+        closeAllDropdowns();
+    }
+}
+
+// close all dropdowns
+function closeAllDropdowns() {
+    sidebarDropDownButtons.forEach((btn) => {
+        btn.parentElement.classList.remove("active");
+        countDropDownContainerHeight(btn.parentElement);
+    });
+}
+
+// update sidebar active class
+function updateSidebarActiveClass() {
+    // check if any dropdwon is open
+    const isActive = Array.from(sidebarDropDownButtons).some((btn) =>
+        btn.parentElement.classList.contains("active")
+    );
+    // open the sidebar if any dropdown is open
+    sidebar.classList.toggle("active", isActive);
+}
+
+// calculate dropdown container height
+function countDropDownContainerHeight(dropDownContainer) {
+    const dropDownBody = dropDownContainer.querySelector(".GpIbu");
+    const dropDownItems = dropDownBody.children;
+    let height = 0;
+
+    for (let i = 0; i < dropDownItems.length; i++) {
+        height += dropDownItems[i].offsetHeight;
+    }
+
+    // add height to dropdown if open
+    if (dropDownContainer.classList.contains("active")) {
+        dropDownBody.style.height = height + "px";
+    } else {
+        // remove height if dropdown is close
+        dropDownBody.style.height = 0;
+    }
+}
---
+++ single-menu-dropdown.js
@@ -0,0 +1,65 @@
+// handle menu dropdown
+function singleMenu(targetId, menuId, show = false) {
+    const targetElement = document.getElementById(targetId);
+    const menuElement = document.getElementById(menuId);
+
+    // Initial state
+    if (show) {
+        // show dropdown
+        menuElement.style.display = "block";
+        targetElement.classList.add("active");
+    } else {
+        // hide dropdown
+        menuElement.style.display = "none";
+        targetElement.classList.remove("active");
+    }
+
+    // Toggle menu visibility when target element is clicked
+    targetElement.addEventListener("click", () => {
+        show = !show;
+
+        if (show) {
+            // show dropdown
+            menuElement.style.display = "block";
+            targetElement.classList.add("active");
+        } else {
+            // hide dropdown
+            menuElement.style.display = "none";
+            targetElement.classList.remove("active");
+        }
+    });
+
+    // Close menu if clicked outside of container
+    document.addEventListener("click", (event) => {
+        if (!targetElement.contains(event.target)) {
+            show = false;
+            menuElement.style.display = "none";
+            targetElement.classList.remove("active");
+        }
+    });
+
+    // Prevent menu from closing when clicked inside the menu element
+    menuElement.addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+}
+
+// Call singleMenu function for each menu
+singleMenu("target_id1", "menu_id1", false);
+singleMenu("target_id2", "menu_id2", false);
+singleMenu("target_id3", "menu_id3", false);
+
+// Check the containers position to align the menus
+const dropdownContainers = document.querySelectorAll(".target-id");
+const dropdownMenus = document.querySelectorAll(".menu-id");
+
+dropdownContainers.forEach((container) => {
+    const rect = container.getBoundingClientRect();
+    const viewportWidth = window.innerWidth;
+    const dropdownMenu = container.querySelector(".menu-id");
+
+    // Add 'right' class if the dropdown menu is within 300px from the right edge of the screen
+    if (rect.right > viewportWidth - 300) {
+        dropdownMenu.classList.add("right");
+    }
+});
