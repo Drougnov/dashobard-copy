d62be0b1a77273be6e5426afbbc1d4a68e254458
RockyBaura <251drougnov@gmail.com>
2023-07-20 08:30:15 +0600

    toggle slide-in menu with hamburger click

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
-const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
 const hamburgerMenu = document.querySelector(".nav-menu");
 
 // add class 'active' to nav on hamburger menu click
 hamburgerMenu.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+    header.classList.toggle("nav-active");
 });
