7340eae6a4a9359b8d10b20f04e3e73f3e6b0154
RockyBaura <251drougnov@gmail.com>
2023-08-01 13:24:26 +0600

    Cart responsiveness updated

--- cart.js
+++ cart.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // --------------Move coupon out of table----------------------
     // Find the first form with the class "woocommerce-cart-form"
     const form = document.querySelector("form.woocommerce-cart-form");
 
@@ -22,4 +23,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Remove the original second tr from the table
     secondTR.remove();
+
+    // --------------Move notice out of woocommerce----------------------
+
+    // Find the elements with their class names
+    const pageContent = document.querySelector(".page-content");
+    const woocommerce = pageContent.querySelector(".woocommerce");
+    const noticesWrapper = woocommerce.querySelector(
+        ".woocommerce-notices-wrapper"
+    );
+
+    // Remove the noticesWrapper from its original position
+    woocommerce.removeChild(noticesWrapper);
+
+    // Insert the noticesWrapper before the woocommerce div
+    pageContent.insertBefore(noticesWrapper, woocommerce);
+});
+
+// On quantity change call update button
+document.addEventListener("DOMContentLoaded", function () {
+    const inputElement = document.querySelector(".qty.text");
+    const updateButton = document.querySelector('button[name="update_cart"]');
+
+    inputElement.addEventListener("change", function () {
+        updateButton.click();
+        const newDiv = document.createElement("div");
+
+        // Move the contents of the second tr to the new div
+        newDiv.innerHTML = secondTR.innerHTML;
+
+        // Add a class to the new divs
+        newDiv.classList.add("coupon_wrapper");
+
+        // Append the new div to the form, right after the table
+        form.insertBefore(newDiv, table.nextSibling);
+
+        // Remove the original second tr from the table
+        secondTR.remove();
+    });
 });
