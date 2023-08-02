91c19c93d7d136d9d54281539c6abaa147345866
RockyBaura <251drougnov@gmail.com>
2023-08-02 09:47:49 +0600

    Header dropdown updated

--- header.js
+++ header.js
@@ -5,3 +5,10 @@ const hamburgerMenu = document.querySelector(".nav-menu");
 hamburgerMenu.addEventListener("click", () => {
     header.classList.toggle("nav-active");
 });
+
+// remove class 'active' to nav on close button click
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", () => {
+    header.classList.toggle("nav-active");
+});
